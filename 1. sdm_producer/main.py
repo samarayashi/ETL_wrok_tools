6f6f1a0fbeb32fbd1bb9f9b5f5f7a1370e4633f8
samarayashi <68182100+samarayashi@users.noreply.github.com>
2022-06-16 17:24:50 +0800

    Update main.py
    fix some detail, like output filename , call write function in SdmProducer class.

--- 1. sdm_producer/main.py
+++ 1. sdm_producer/main.py
@@ -1,6 +1,7 @@
 from raw_sql import ColumnTextHandler
 from column_info import DecodeColumnHandler
-from sdm import SdmProducer, write_sdm_csv, write_ddl_txt, write_sensitive_check
+from sdm import SdmProducer
+
 
 test_sql = input_raw_sql = """ 
 INSERT INTO TMP_TEST_PART1
@@ -13,12 +14,15 @@ column_handler = ColumnTextHandler(test_sql)
 with DecodeColumnHandler('tables_information.db', column_handler) as column_decoder:
     detail_list = column_decoder.get_column_detail_list()
     new_table_name = column_decoder.write_back_sqlite(detail_list, disinherit_columns=['not_null', 'is_pk'])
+    print("raw_sql\n", column_handler.raw_sql)
     print('new_table_name:' + new_table_name)
+    input(f"Make sure the column info of {new_table_name} is correct in sqlite\n"
+          f"Press Enter to produce SDM and SQL: ")
 
 with SdmProducer('tables_information.db') as sdm_producer:
     ddl_text = sdm_producer.get_create_dll(new_table_name)
-    sdm_content = sdm_producer.get_sdm_content('wf_test', new_table_name)
+    sdm_content = sdm_producer.get_sdm_content(f'wf_{new_table_name}', new_table_name)
     sensitive_dict = sdm_producer.get_sensitive_column(new_table_name)
-    write_ddl_txt('test_ddl.sql', ddl_text)
-    write_sdm_csv('test_sdm.csv', sdm_content)
-    write_sensitive_check('test_sensitive.csv', sensitive_dict)
+    sdm_producer.write_ddl_txt(f'{new_table_name}_ddl.sql', ddl_text)
+    sdm_producer.write_sdm_csv(f'{new_table_name}_sdm.csv', sdm_content)
+    sdm_producer.write_sensitive_check(f'{new_table_name}_sensitive.csv', sensitive_dict)
